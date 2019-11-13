e415d93ea3dac7b38ec72b2bbba6f4a77d85794f
Mark Lynch <lynchm3@tcd.ie>
2019-11-13 20:03:58 +0000

    Json classes moved to data package

--- app/src/main/java/com/marklynch/currencyfair/livedata/flickr/data/FlickrSearchResponse.java
+++ app/src/main/java/com/marklynch/currencyfair/livedata/flickr/data/FlickrSearchResponse.java
@@ -1,4 +1,4 @@
-package com.marklynch.currencyfair.livedata.flickr;
+package com.marklynch.currencyfair.livedata.flickr.data;
 
 //{"photos":
 //        {"page":1,"pages":2236,"perpage":100,"total":"223547","photo":
--- app/src/main/java/com/marklynch/currencyfair/livedata/flickr/data/Photos.java
+++ app/src/main/java/com/marklynch/currencyfair/livedata/flickr/data/Photos.java
@@ -1,4 +1,4 @@
-package com.marklynch.currencyfair.livedata.flickr;
+package com.marklynch.currencyfair.livedata.flickr.data;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
