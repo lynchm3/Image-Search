989c31a6687ed27a94d36ec0b0850712f7417d7f
Mark Lynch <lynchm3@tcd.ie>
2019-11-17 22:27:20 +0000

    Unit test for search

--- app/src/androidTest/java/com/marklynch/currencyfair/EspressoWrapper.java
+++ app/src/androidTest/java/com/marklynch/currencyfair/EspressoWrapper.java
@@ -1,4 +1,93 @@
 package com.marklynch.currencyfair;
 
+import android.app.Activity;
+import android.content.Context;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+
+import androidx.appcompat.widget.SearchView;
+import androidx.test.espresso.UiController;
+import androidx.test.espresso.ViewAction;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.matcher.ViewMatchers;
+
+import org.hamcrest.Matcher;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
+import static androidx.test.espresso.action.ViewActions.pressKey;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.AllOf.allOf;
+import static org.junit.Assert.assertTrue;
+
 public class EspressoWrapper {
+
+    public static void assertViewVisible(int viewId) {
+        onView(withId(viewId)).check(
+                matches(
+                        withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)
+                )
+        );
+    }
+
+    public static void assertKeyboardShown(Activity activity) {
+        InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+        assertTrue(inputMethodManager.isAcceptingText());
+    }
+
+    public static void clickViewWithId(int viewId) {
+        onView(withId(viewId)).perform(click());
+    }
+
+    public static void clickViewWithText(String viewText) {
+        onView(withText(viewText)).perform(click());
+    }
+
+    public static void typeTextInEditText(int viewId, String text) {
+        onView(withId(viewId)).perform(typeText(text));
+    }
+
+    public static void clickIMEActionButtonForEditText(int viewId) {
+        onView(withId(viewId))
+                .perform(pressImeActionButton());
+    }
+
+    public static void typeTheLetterA()
+    {
+        onView(isRoot()).perform(pressKey(KeyEvent.KEYCODE_A));
+    }
+
+    public static void typeTextInSearchVIew(int viewId, String text) {
+        onView(withId(viewId))
+                .perform(typeSearchViewText(text));
+    }
+
+    public static ViewAction typeSearchViewText(final String text) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return allOf(isDisplayed(), isAssignableFrom(SearchView.class));
+            }
+
+            @Override
+            public String getDescription() {
+                return "Change view text";
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                ((SearchView) view).setQuery(text, false);
+            }
+        };
+    }
 }
--- app/src/androidTest/java/com/marklynch/currencyfair/ui/main/MainActivityTest.java
+++ app/src/androidTest/java/com/marklynch/currencyfair/ui/main/MainActivityTest.java
@@ -1,13 +1,14 @@
 package com.marklynch.currencyfair.ui.main;
 
-import android.content.Context;
+import android.app.Activity;
 import android.content.res.Resources;
-import android.view.inputmethod.InputMethodManager;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.widget.SearchView;
-import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
-import androidx.test.espresso.matcher.ViewMatchers;
 
 import com.marklynch.currencyfair.MainActivity;
 import com.marklynch.currencyfair.R;
@@ -16,9 +17,12 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static com.marklynch.currencyfair.EspressoWrapper.assertKeyboardShown;
+import static com.marklynch.currencyfair.EspressoWrapper.assertViewVisible;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -30,27 +34,45 @@ public class MainActivityTest {
     public Resources resources = getInstrumentation().getTargetContext().getResources();
 
     @Test
-    public void checkInitialState() {
+    public void testInitialState() {
         activityTestRule.launchActivity(null);
+        initialStateChecks();
+        activityTestRule.finishActivity();
+    }
+
+    @Test
+    public void testSearch() throws InterruptedException {
+        Activity activity = activityTestRule.launchActivity(null);
+
+        initialStateChecks();
+
+        onView(isAssignableFrom(EditText.class)).perform(typeText("Puppy"), pressImeActionButton());
+
+        RelativeLayout searchProgressBar = activity.findViewById(R.id.search_progress_bar);
+        while (searchProgressBar.getVisibility() == View.VISIBLE) {
+            Thread.sleep(1_000);
+        }
+
+        RecyclerView recyclerView = activity.findViewById(R.id.recycler_view);
+
+        activity.runOnUiThread(() -> recyclerView.scrollBy(0, 10_000));
+
+        Thread.sleep(1_000);
+
+        RelativeLayout infiniteScrollProgressBar = activity.findViewById(R.id.infinite_scroll_progress_bar);
+        while (infiniteScrollProgressBar.getVisibility() == View.VISIBLE) {
+            Thread.sleep(1_000);
+        }
+
+        activityTestRule.finishActivity();
+    }
+
+    public void initialStateChecks() {
         SearchView searchView = activityTestRule.getActivity().findViewById(R.id.search_view);
         assertEquals("", searchView.getQuery().toString());
         assertEquals(resources.getString(R.string.query_hint), searchView.getQueryHint().toString());
         assertTrue(searchView.hasFocus());
         assertViewVisible(R.id.search_view);
-        assertKeyboardShown();
-        activityTestRule.finishActivity();
-    }
-
-    public void assertViewVisible(int viewId) {
-        onView(withId(viewId)).check(
-                ViewAssertions.matches(
-                        withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)
-                )
-        );
-    }
-
-    public void assertKeyboardShown() {
-        InputMethodManager inputMethodManager = (InputMethodManager) activityTestRule.getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-        assertTrue(inputMethodManager.isAcceptingText());
+        assertKeyboardShown(activityTestRule.getActivity());
     }
 }
